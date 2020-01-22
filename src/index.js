909d1de87b3930370765409f68fad0f91df12d2f
Andreu Santaren Llop <andreu.santaren@gmail.com>
2020-01-22 18:34:12 +0100

    Remove unused code in index.

--- src/index.js
+++ src/index.js
@@ -3,20 +3,4 @@ import Tracker from './tracker'
 
 nrvideo.PrebidTracker = Tracker
 
-//TODO: init tracker using Tracker.init static constructor
-
-/*
-document.addEventListener('readystatechange', (event) => {
-    if (document.readyState === 'complete') {
-        Tracker.init()
-    }    
-})
-
-if (window.addEventListener) {
-    window.addEventListener('load', Tracker.init, false)
-} else if (window.attachEvent) {
-    window.attachEvent('onload', Tracker.init)
-}
-*/
-
 module.exports = nrvideo
