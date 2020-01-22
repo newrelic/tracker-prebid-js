fdca46d733aa4d96df66161e1f8d5eb562bd08ca
Andreu Santaren Llop <andreu.santaren@gmail.com>
2020-01-22 18:16:51 +0100

    Implement event listeners

--- src/tracker.js
+++ src/tracker.js
@@ -7,7 +7,9 @@ export default class PrebidTracker extends nrvideo.Tracker {
    * @static
    * @returns {object} Tracker reference.
    */
-  static init () {
+  static init (pbjs) {
+    nrvideo.Log.debug('PrebidTracker init ', pbjs)
+    
     let trackers = nrvideo.Core.getTrackers()
     for (let i = 0 ; i < trackers.length ; i++) {
       if (trackers[i] instanceof PrebidTracker) {
@@ -17,7 +19,7 @@ export default class PrebidTracker extends nrvideo.Tracker {
 
     let tracker = new PrebidTracker()
     nrvideo.Core.addTracker(tracker)
-    tracker.registerListeners()
+    tracker.registerListeners(pbjs)
     return tracker
   }
 
@@ -28,8 +30,6 @@ export default class PrebidTracker extends nrvideo.Tracker {
     super(options)
 
     this.reset()
-
-    this.slots = {}
   }
 
   /** Resets all flags and chronos. */
@@ -54,25 +54,104 @@ export default class PrebidTracker extends nrvideo.Tracker {
   }
 
   /**
-   * Returns given slot info, or creates a new one.
-   * @param {string} slotId Unique slot id.
+   * Register listeners.
    */
-  getSlotState (slotId) {
-    if (!this.slots[slotId]) { // first time
-      this.slots[slotId] = {
-        chrono: new nrvideo.Chrono(),
-        visible: false
-      }
-    }
-    return this.slots[slotId]
+  registerListeners (pbjs) {
+    pbjs.onEvent('auctionInit', this.onAuctionInit)
+    pbjs.onEvent('auctionEnd', this.onAuctionEnd)
+    pbjs.onEvent('bidAdjustment', this.onBidAdjustment)
+    pbjs.onEvent('bidTimeout', this.onBidTimeout)
+    pbjs.onEvent('bidRequested', this.onBidRequested)
+    pbjs.onEvent('bidResponse', this.onBidResponse)
+    pbjs.onEvent('bidWon', this.onBidWon)
+    pbjs.onEvent('setTargeting', this.onSetTargeting)
+    pbjs.onEvent('requestBids', this.onRequestBids)
+    pbjs.onEvent('addAdUnits', this.onAddAdUnits)
+    pbjs.onEvent('adRenderFailed', this.onAdRenderFailed)
+    pbjs.onEvent('bidderDone', this.onBidderDone)
   }
 
   /**
-   * Register listeners.
+   * Called once Prebid fires 'auctionInit' event.
    */
-  registerListeners () {
-    // TODO: register event listeners
+  onAuctionInit () {
+    nrvideo.Log.debug('onAuctionInit')
   }
 
-  //TODO: event listeners
+  /**
+   * Called once Prebid fires 'auctionEnd' event.
+   */
+  onAuctionEnd () {
+    nrvideo.Log.debug('onAuctionEnd')
+  }
+
+  /**
+   * Called once Prebid fires 'bidAdjustment' event.
+   */
+  onBidAdjustment () {
+    nrvideo.Log.debug('onBidAdjustment')
+  }
+
+  /**
+   * Called once Prebid fires 'bidTimeout' event.
+   */
+  onBidTimeout () {
+    nrvideo.Log.debug('onBidTimeout')
+  }
+
+  /**
+   * Called once Prebid fires 'bidRequested' event.
+   */
+  onBidRequested () {
+    nrvideo.Log.debug('onBidRequested')
+  }
+
+  /**
+   * Called once Prebid fires 'bidResponse' event.
+   */
+  onBidResponse () {
+    nrvideo.Log.debug('onBidResponse')
+  }
+
+  /**
+   * Called once Prebid fires 'bidWon' event.
+   */
+  onBidWon () {
+    nrvideo.Log.debug('onBidWon')
+  }
+
+  /**
+   * Called once Prebid fires 'setTargeting' event.
+   */
+  onSetTargeting () {
+    nrvideo.Log.debug('onSetTargeting')
+  }
+
+  /**
+   * Called once Prebid fires 'requestBids' event.
+   */
+  onRequestBids () {
+    nrvideo.Log.debug('onRequestBids')
+  }
+
+  /**
+   * Called once Prebid fires 'addAdUnits' event.
+   */
+  onAddAdUnits () {
+    nrvideo.Log.debug('onAddAdUnits')
+  }
+
+  /**
+   * Called once Prebid fires 'adRenderFailed' event.
+   */
+  onAdRenderFailed () {
+    nrvideo.Log.debug('onAdRenderFailed')
+  }
+
+  /**
+   * Called once Prebid fires 'bidderDone' event.
+   */
+  onBidderDone () {
+    nrvideo.Log.debug('onBidderDone')
+  }
 }
